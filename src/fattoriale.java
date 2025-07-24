1f470d0054d0796f8c6766ed1446afe50eef0eb8
CarlottaPolito <carlottapolitowd@gmail.com>
2025-07-24 19:32:14 +0200

    add somma e fattoriale

---
+++ src/fattoriale.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fattoriale {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        //chiedere un numero all'utente
+        System.out.print("Inserire il numero di cui si vuole calcolare il fattoriale: ");
+        int numero = input.nextInt();
+
+        //calcolare il suo fattoriale
+        int fattoriale = 1;
+        for (int i = numero; i >0; i--) {
+
+        }
+    }
+}
---
+++ src/somma.java
@@ -0,0 +1,9 @@
+public class somma {
+    public static void main(String[] args) {
+        int [] numeri = {1, 2, 3, 4, 5};
+
+        //calcola somma di elementi dell'array
+
+        
+    }
+}
